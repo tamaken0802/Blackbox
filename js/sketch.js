cee129c8f97abc7a854a22447bbe06e7758975d1
tamaken0802 <85724739+tamaken0802@users.noreply.github.com>
2021-12-13 15:43:09 +0900

    a

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,9 @@
-function setup() { 
-    createCanvas(400, 400);
-  } 
-  
-  function draw() { 
-    background(220);
-  }
+function setup(){
+  // 描画領域のセットアップ
+  createCanvas(800, 500);  //サイズ: 800px × 500px
+  background(0);           //背景色: 黒
+}
+
+function draw(){
+  // 描画
+}
